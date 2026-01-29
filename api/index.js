ea710a18f0965504b6fa96a40b95c06813857f52
Ahmed Hassan Awan <awanhasan2007@gmail.com>
2026-01-29 19:20:12 +0500

    fix: add app.listen for production

--- api/index.js
+++ api/index.js
@@ -46,13 +46,15 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() =>    console.log("✅ MongoDB Connected   database" , process.env.MONGO_URI))
   .catch((err) => console.error(" MongoDB Error:", err));
 
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log(` Server running on port ${process.env.PORT || 3000}`);
-// });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
+
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
   
 
-export default app;
