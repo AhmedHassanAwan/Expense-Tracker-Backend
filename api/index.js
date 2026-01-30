565198a6541b24d2f4f6e709eb9b37e67ba4e471
Ahmed Hassan Awan <awanhasan2007@gmail.com>
2026-01-30 10:46:05 +0500

    new frontend url added in backend

--- api/index.js
+++ api/index.js
@@ -26,7 +26,8 @@ app.use(cors({
   origin: [
     "http://localhost:5174", 
     "http://localhost:5173",
-    "https://frontend-expense-chi.vercel.app"
+    "https://frontend-expense-chi.vercel.app",
+    "https://frontend-theta-navy-74.vercel.app",
   ],
   credentials: true
 }));
